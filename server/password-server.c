ec2c9661a692cf8b17d51430ef3e94e4d122edbb
Tapiwanashe B. Zvidzwa <zvidzwat@cs.grinnell.edu>
2018-05-10 15:02:49 -0500

    added slices

--- server/password-server.c
+++ server/password-server.c
@@ -10,9 +10,12 @@
 #include <netinet/in.h>
 #include <sys/time.h>
 #include <arpa/inet.h>
+#include <math.h>
 
 #define SERVER_PORT 4444
-#define NUM_SLICES 26
+#define NUM_SLICES 1024
+#define END_NUM 8031810176
+
 
 bool CLIENT_FOUND = false;
 int s = 0;
@@ -24,6 +27,13 @@ typedef enum {
   KEEP_LOOKING
 } response_t;
 
+//slice type
+typedef struct slice {
+  double start;
+  double end;
+  int client_id;
+  bool explored;
+} slice_t;
 
 //Data packets sent between client and server
 typedef struct packet {
@@ -31,6 +41,7 @@ typedef struct packet {
 	double ending; 
 	int client_id;
 	char password [8];
+  char hash [33];
 	response_t response;
 } packet_t;
 
@@ -68,6 +79,96 @@ typedef struct thread_arg {
 
 
 threads_list_t* threads_list;
+slice_t slices[NUM_SLICES];
+char HASH[] = "e2efd0c1309976a8a2aa5dec92d435cb";
+
+//function signatures
+int get_search_space(); 
+void append_thread_node(pthread_t thread);
+void join_threads(); 
+void append_node(int client_id, int port, char* ipstr);
+void init_client_list();
+void init_thread_list();
+void init_slices(); 
+void shut_down(); 
+void* time_out_fn(void* p);
+void* client_thread_fn(void* p);
+int get_search_space(); 
+
+
+//Main Driver function
+int main() {
+  // Set up a socket
+  s = socket(AF_INET, SOCK_STREAM, 0);
+
+  if(s == -1) {
+    perror("socket");
+    exit(2);
+  }
+
+  // Listen at this address. We'll bind to port 0 to accept any available port
+  struct sockaddr_in addr = {
+    .sin_addr.s_addr = INADDR_ANY,
+    .sin_family = AF_INET,
+    .sin_port = htons(SERVER_PORT)
+  };
+
+  // Bind to the specified address
+  if(bind(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))) {
+    perror("bind");
+    exit(2);
+  }
+
+  // Become a server socket
+  listen(s, 2);
+  
+  // Get the listening socket info so we can find out which port we're using
+  socklen_t addr_size = sizeof(struct sockaddr_in);
+  getsockname(s, (struct sockaddr *) &addr, &addr_size);
+  
+  // Print the port information
+  printf("Listening on port %d\n", ntohs(addr.sin_port));
+  
+  int client_count = 0;
+  init_thread_list();
+  init_client_list();
+  init_slices();
+
+  // Repeatedly accept connections
+  while(true) {
+    // Accept a client connection
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(struct sockaddr_in);
+
+    int client_socket = accept(s, (struct sockaddr*)&client_addr, &client_addr_len);
+    
+    char ipstr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, ipstr, INET_ADDRSTRLEN);
+    
+    printf("Client %d connected from %s\n", client_count, ipstr);
+    
+    // Set up arguments for the client thread
+    thread_arg_t* args = malloc(sizeof(thread_arg_t));
+    args->socket_fd = client_socket;
+    args->client_number = client_count;
+    
+    // Create the thread
+    pthread_t thread;
+    if(pthread_create(&thread, NULL, client_thread_fn, args)) {
+      perror("pthread_create failed");
+      exit(EXIT_FAILURE);
+    }
+
+    append_node(client_count, client_socket, ipstr);
+    append_thread_node(thread);
+    client_count++;
+  }
+ 
+ return 0;
+}
+
+
+//METHOD IMPLEMENTATIONS
 
 void append_thread_node(pthread_t thread) {
 	//allocate memory for the new thread
@@ -149,11 +250,14 @@ void shut_down() {
 }
 
 void* time_out_fn(void* p) {
+
 printf("Waiting for remaining clients now\n");
 sleep(15);
 printf("TIME OUT FINISHED, SERVER SHUTTING DOWN\n");
 shut_down();
 return NULL;
+
+
 }
 
 void* client_thread_fn(void* p) {
@@ -170,10 +274,18 @@ void* client_thread_fn(void* p) {
     exit(EXIT_FAILURE);
   }
 
+  //Give the client initial search space
+  int index;   
+  if((index = get_search_space()) == -1) {
+        perror("Something went wrong with getting seach space\n");
+        exit(EXIT_FAILURE);
+    }
+
   packet_t packet;
-  packet.starting = 10;
-  packet.ending = 20;
+  packet.starting = slices[index].start;
+  packet.ending = slices[index].end;
   packet.client_id = client_number;
+  strcpy(packet.hash, HASH);
   packet.response = KEEP_LOOKING;
 
   write(socket_fd_copy, &packet, sizeof(packet_t));
@@ -198,8 +310,15 @@ void* client_thread_fn(void* p) {
     		}
             break;
           case REQUEST_MORE: 
-            packet.starting = 10;
-            packet.ending = 20;
+              
+              if((index = get_search_space()) == -1) {
+            perror("Something went wrong with getting seach space\n");
+            exit(EXIT_FAILURE);
+          }
+
+            strcpy(packet.hash, HASH);
+            packet.starting = slices[index].start;
+            packet.ending = slices[index].end;
             packet.client_id = client_number;
             packet.response = KEEP_LOOKING;
             write(socket_fd_copy, &packet, sizeof(packet_t));
@@ -215,73 +334,35 @@ void* client_thread_fn(void* p) {
   return NULL;
 }
 
-int main() {
-  // Set up a socket
-  s = socket(AF_INET, SOCK_STREAM, 0);
 
-  if(s == -1) {
-    perror("socket");
-    exit(2);
+///////////// NEW CODE
+void init_slices() {
+  double SLICE_SIZE = ceil(END_NUM / NUM_SLICES);
+  double last_end = 0;
+
+  for(int i = 0; i <= NUM_SLICES; i++) {
+    slice_t slice;
+    slice.start = i * SLICE_SIZE;
+    slice.end = slice.start + SLICE_SIZE - 1;
+    slice.explored = false;
+    slice.client_id = 0;
+    slices[i] = slice;
   }
-
-  // Listen at this address. We'll bind to port 0 to accept any available port
-  struct sockaddr_in addr = {
-    .sin_addr.s_addr = INADDR_ANY,
-    .sin_family = AF_INET,
-    .sin_port = htons(SERVER_PORT)
-  };
-
-  // Bind to the specified address
-  if(bind(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in))) {
-    perror("bind");
-    exit(2);
-  }
-
-  // Become a server socket
-  listen(s, 2);
-  
-  // Get the listening socket info so we can find out which port we're using
-  socklen_t addr_size = sizeof(struct sockaddr_in);
-  getsockname(s, (struct sockaddr *) &addr, &addr_size);
-  
-  // Print the port information
-  printf("Listening on port %d\n", ntohs(addr.sin_port));
-  
-  int client_count = 0;
-  init_thread_list();
-  init_client_list();
-
-  // Repeatedly accept connections
-  while(true) {
-    // Accept a client connection
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(struct sockaddr_in);
-
-    int client_socket = accept(s, (struct sockaddr*)&client_addr, &client_addr_len);
-    
-    char ipstr[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &client_addr.sin_addr, ipstr, INET_ADDRSTRLEN);
-    
-    printf("Client %d connected from %s\n", client_count, ipstr);
-    
-    // Set up arguments for the client thread
-    thread_arg_t* args = malloc(sizeof(thread_arg_t));
-    args->socket_fd = client_socket;
-    args->client_number = client_count;
-    
-    // Create the thread
-    pthread_t thread;
-    if(pthread_create(&thread, NULL, client_thread_fn, args)) {
-      perror("pthread_create failed");
-      exit(EXIT_FAILURE);
-    }
-
-    append_node(client_count, client_socket, ipstr);
-    append_thread_node(thread);
-    client_count++;
-  }
- 
- return 0;
 }
+
+//Returns the index of an unexplored search space
+int get_search_space() {
+  //TODO WE NEED TO INCLUDE A LOCK HERE
+  for(int i = 0; i < NUM_SLICES; i++) {
+    if(!slices[i].explored) {
+      slices[i].explored = true;
+      return i;
+    }
+  }
+
+  return -1;
+  //TODO WE NEED TO UNLOCK HERE
+}
+
 
 
